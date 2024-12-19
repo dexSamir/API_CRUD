a8865907f85df816eaef4b7438e908e044c991fe
xaqan05 <xaqan2005@gmail.com>
2024-12-19 19:48:23 +0400

    add migrations and create first api example

---
+++ CRUD-Task/DAL/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using CRUD_Task.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Task.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Language> Languages { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Language>(b =>
+            {
+                b.HasKey(x => x.Code);
+                b.Property(x => x.Code)
+                .HasMaxLength(2); 
+                b.HasIndex(x => x.Name)
+                    .IsUnique();
+                b.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(32);
+                b.Property(x => x.Icon)
+                   .HasMaxLength(128);
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
